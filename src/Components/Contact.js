f8ce8440e43e6f66388e7d9ed979dd3a764b2196
MairaGalvao <mairagalvao01@gmail.com>
2025-01-27 15:13:21 +0200

    refactor

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,37 +1,37 @@
-import React, { useState, useEffect, useRef } from "react";
-import contactPic from "../images/contactPic.jpg";
-import { Box } from "@material-ui/core";
-import { send } from "emailjs-com";
-import { init } from "emailjs-com";
-import Aos from "aos";
-import "aos/dist/aos.css";
-import "../css/contact.css";
-import { saveAs } from "file-saver";
+import React, { useState, useEffect, useRef } from 'react';
+import contactPic from '../images/contactPic.jpg';
+import { Box } from '@material-ui/core';
+import { send } from 'emailjs-com';
+import { init } from 'emailjs-com';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import '../css/contact.css';
+import { saveAs } from 'file-saver';
 
-init("user_gqMmkiihxMb3zWbl0pRBo");
+init('user_gqMmkiihxMb3zWbl0pRBo');
 
 export function Contact() {
   const form = useRef();
 
   const onSubmit = (e) => {
     e.preventDefault();
-    send("service_3nir9cd", "template_43w1rg5", toSend, "Au4ZJV5hRzPj2cSXG")
+    send('service_3nir9cd', 'template_43w1rg5', toSend, 'Au4ZJV5hRzPj2cSXG')
       .then((response) => {
-        console.log("SUCCESS!", response.status, response.text);
+        console.log('SUCCESS!', response.status, response.text);
       })
       .catch((err) => {
-        console.log("FAILED...", err);
+        console.log('FAILED...', err);
       });
   };
   const [toSend, setToSend] = useState({
-    from_name: "",
-    message: "",
-    reply_to: "",
+    from_name: '',
+    message: '',
+    reply_to: '',
   });
   const saveFile = () => {
     saveAs(
-      "https://www.figma.com/design/b6SURXWqNx8cnaU3uJ3iF6/Maira-Galvao-Software-Engineer?node-id=0-1&t=7Fb3yz6yEYEw3ajU-1",
-      "Maira Galvao Software Engineer.pdf"
+      'https://www.figma.com/design/b6SURXWqNx8cnaU3uJ3iF6/Maira-Galvao-Software-Engineer?node-id=0-1&t=7Fb3yz6yEYEw3ajU-1',
+      'Maira Galvao Software Engineer.pdf'
     );
   };
   const handleChange = (e) => {
@@ -47,7 +47,7 @@ export function Contact() {
     <>
       <div class="form-box" id="contact">
         <form id="contact_form" ref={form} onSubmit={onSubmit}>
-          <h2 style={{ paddingBottom: "10px" }}>Keep in touch</h2>
+          <h2 style={{ paddingBottom: '10px' }}>Keep in touch</h2>
 
           <label>Name</label>
           <input
@@ -86,10 +86,10 @@ export function Contact() {
           <img
             id="picContact"
             style={{
-              float: "right",
+              float: 'right',
 
-              paddingRight: "0",
-              width: "60%",
+              paddingRight: '0',
+              width: '60%',
             }}
             src={contactPic}
           />
